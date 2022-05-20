cf82dc2c109561afec14e854acdccdfe869f6694
Colin Simon <s81601@htw-dresden.de>
2022-05-20 10:23:07 +0200

    again

--- pytorch-cifar/main.py
+++ pytorch-cifar/main.py
@@ -1,4 +1,4 @@
-'''Train CIFAR10 with PyTorch.'''
+"""Train CIFAR10 with PyTorch."""
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -22,37 +22,49 @@ from models import *
 from utils import progress_bar
 
 BATCH_SIZE = 128
-ckpt_filename = 'ckpt.pth'
-data_dir = './data'
+ckpt_filename = "ckpt.pth"
+data_dir = "./data"
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--epochs', default=200, type=int, help='how many epochs should the net train for')
-parser.add_argument('--resume', '-r', action='store_true',
-                    help='resume from checkpoint')
-parser.add_argument('--deepaugment', '-da', action='store_true',
-                    help='use deepaugment policy for data augmentation place policy.txt next to this file')
-parser.add_argument('--randaugment', '-ra', action='store_true',
-                    help='use randaugment transformer for data augmentation')
+parser = argparse.ArgumentParser(description="PyTorch CIFAR10 Training")
+parser.add_argument("--lr", default=0.1, type=float, help="learning rate")
+parser.add_argument(
+    "--epochs", default=200, type=int, help="how many epochs should the net train for"
+)
+parser.add_argument(
+    "--resume", "-r", action="store_true", help="resume from checkpoint"
+)
+parser.add_argument(
+    "--deepaugment",
+    "-da",
+    action="store_true",
+    help="use deepaugment policy for data augmentation place policy.txt next to this file",
+)
+parser.add_argument(
+    "--randaugment",
+    "-ra",
+    action="store_true",
+    help="use randaugment transformer for data augmentation",
+)
 args = parser.parse_args()
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
 # Data
-print('==> Preparing data..')
+print("==> Preparing data..")
 
 if args.deepaugment:
-    ckpt_filename = 'deepaugment_' + ckpt_filename
-    trainset = torchvision.datasets.CIFAR10(
-        root=data_dir, train=True, download=True)
+    ckpt_filename = "deepaugment_" + ckpt_filename
+    trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True, download=True)
     numpy_X = trainset.data
     numpy_y = np.asarray(trainset.targets)
-    policies_df = pd.read_csv('deepaug_policies.csv')
+    policies_df = pd.read_csv("deepaug_policies.csv")
     policies_list = load_k_policies_from_csv(policies_df, k=20)
-    image_gen = deepaugment_image_generator(numpy_X, numpy_y, policies_list, batch_size=1, show_policy=True)
-    data_dir = './data/cifar-10-deepaugment'
+    image_gen = deepaugment_image_generator(
+        numpy_X, numpy_y, policies_list, batch_size=1, show_policy=True
+    )
+    data_dir = "./data/cifar-10-deepaugment"
     if not os.path.isdir(data_dir):
         os.mkdir(data_dir)
 
@@ -60,84 +72,100 @@ if args.deepaugment:
     for image, label in image_gen:
         # print('test')
         label = label[0]
-        if not os.path.isdir(data_dir + '/' + str(label)):
-            os.mkdir(data_dir + '/' + str(label))
+        if not os.path.isdir(data_dir + "/" + str(label)):
+            os.mkdir(data_dir + "/" + str(label))
         # Image.fromarray(image[0]).save(data_dir + '/' + str(label) + '/' + str(label) + '_' + str(counter) + '.jpeg')
         counter += 1
 
-    transform_train = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-    trainset = datasets.ImageFolder(
-        data_dir, transform_train
+    transform_train = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ]
     )
+    trainset = datasets.ImageFolder(data_dir, transform_train)
 
 if args.baseline:
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
+    transform_train = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ]
+    )
     trainset = torchvision.datasets.CIFAR10(
-        root='./data', train=True, download=True, transform=transform_train)
+        root="./data", train=True, download=True, transform=transform_train
+    )
 
 if args.randaugment:
-    ckpt_filename = 'randaugment_' + ckpt_filename
-    transform_train = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(25),
-        transforms.RandAugment(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-    ])
+    ckpt_filename = "randaugment_" + ckpt_filename
+    transform_train = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(25),
+            transforms.RandAugment(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ]
+    )
     trainset = torchvision.datasets.CIFAR10(
-        root=data_dir, train=True, download=True, transform=transform_train)
+        root=data_dir, train=True, download=True, transform=transform_train
+    )
 
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=BATCH_SIZE, shuffle=True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+transform_test = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ]
+)
 
 testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False)
+    root="./data", train=False, download=True, transform=transform_test
+)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+classes = (
+    "plane",
+    "car",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+)
 
 # Model
-print('==> Building model..')
+print("==> Building model..")
 net = ResNet18()
 
 net = net.to(device)
-if device == 'cuda':
+if device == "cuda":
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 
 if args.resume:
     # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
-    net.load_state_dict(checkpoint['net'])
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
+    print("==> Resuming from checkpoint..")
+    assert os.path.isdir("checkpoint"), "Error: no checkpoint directory found!"
+    checkpoint = torch.load("./checkpoint/ckpt.pth")
+    net.load_state_dict(checkpoint["net"])
+    best_acc = checkpoint["acc"]
+    start_epoch = checkpoint["epoch"]
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                      momentum=0.9, weight_decay=5e-4)
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
 
 # Training
 def train(epoch):
-    print('\nEpoch: %d' % epoch)
+    print("\nEpoch: %d" % epoch)
     net.train()
     train_loss = 0
     correct = 0
@@ -155,8 +183,12 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+        progress_bar(
+            batch_idx,
+            len(trainloader),
+            "Loss: %.3f | Acc: %.3f%% (%d/%d)"
+            % (train_loss / (batch_idx + 1), 100.0 * correct / total, correct, total),
+        )
 
 
 def test(epoch):
@@ -176,21 +208,30 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+            progress_bar(
+                batch_idx,
+                len(testloader),
+                "Loss: %.3f | Acc: %.3f%% (%d/%d)"
+                % (
+                    test_loss / (batch_idx + 1),
+                    100.0 * correct / total,
+                    correct,
+                    total,
+                ),
+            )
 
     # Save checkpoint.
-    acc = 100. * correct / total
+    acc = 100.0 * correct / total
     if acc > best_acc:
-        print('Saving..')
+        print("Saving..")
         state = {
-            'net': net.state_dict(),
-            'acc': acc,
-            'epoch': epoch,
+            "net": net.state_dict(),
+            "acc": acc,
+            "epoch": epoch,
         }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/' + ckpt_filename)
+        if not os.path.isdir("checkpoint"):
+            os.mkdir("checkpoint")
+        torch.save(state, "./checkpoint/" + ckpt_filename)
         best_acc = acc
 
 
