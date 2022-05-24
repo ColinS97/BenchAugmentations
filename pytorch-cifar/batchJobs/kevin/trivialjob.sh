84102d4b9f8259ffaf5000daa11aa3a97ef8fd8e
Colin Simon <s81601@htw-dresden.de>
2022-05-24 13:59:26 +0200

    changed job batches

--- pytorch-cifar/batchJobs/kevin/trivialjob.sh
+++ pytorch-cifar/batchJobs/kevin/trivialjob.sh
@@ -7,11 +7,11 @@
 #SBATCH --mem=8000
 #SBATCH --mincpus=4
 #SBATCH --time=08:00:00                             
-#SBATCH --job-name=randaugment
+#SBATCH --job-name=trivialaugment
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=colin.simon@mailbox.tu-dresden.de
-#SBATCH --output=output-randaugment-%j.out
-#SBATCH --error=error-randaugment-%j.err
+#SBATCH --output=output-trivialaugment-%j.out
+#SBATCH --error=error-trivialaugment-%j.err
 
 module --force purge                          				
 module load modenv/hiera CUDA/11.3.1 GCC/11.2.0 Python/3.9.6
@@ -35,7 +35,7 @@ pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 -f https://download.py
 pip install imgaug pandas pytorch-lightning lightning-bolts torchmetrics
 
 
-python main_lightning.py --epochs 10 --randaugment
+python main_lightning.py --epochs 10 --trivialaugment
 
 deactivate
 
