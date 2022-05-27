53dd47d88d5d2db139cb1237aa6b95bb561a198f
Colin Simon <s81601@htw-dresden.de>
2022-05-27 17:15:32 +0200

    changed model conv layer

--- pytorch-cifar/main_lightning_medMNIST.py
+++ pytorch-cifar/main_lightning_medMNIST.py
@@ -153,7 +153,7 @@ test_loader = data.DataLoader(
 
 
 def create_model():
-    model = torchvision.models.resnet18(pretrained=False, num_classes=10)
+    model = torchvision.models.resnet18(pretrained=False, num_classes=n_classes)
     model.conv1 = nn.Conv2d(
         3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False
     )
