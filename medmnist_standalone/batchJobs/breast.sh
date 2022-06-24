ca73e220a98df7ed8d4781d21e5a0c8a0319cdf0
Colin Simon <s81601@htw-dresden.de>
2022-06-24 14:57:45 +0200

    filenames

--- medmnist_standalone/batchJobs/breast.sh
+++ medmnist_standalone/batchJobs/breast.sh
@@ -10,8 +10,8 @@
 #SBATCH --job-name=breast
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=colin.simon@mailbox.tu-dresden.de
-#SBATCH --output=output-randaugment-%j.out
-#SBATCH --error=error-randaugment-%j.out
+#SBATCH --output=output-%j.out
+#SBATCH --error=error-%j.out
 
 module --force purge                          				
 module load modenv/hiera CUDA/11.3.1 GCC/11.2.0 Python/3.9.6
